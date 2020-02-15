3bd8e180b32c1e027ac53ab301d6ad5dfe82df31
djessy.rossi <43148825+djessy1998@users.noreply.github.com>
2020-02-15 21:15:09 +0100

    Pour merge

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -46,6 +46,7 @@ class AppController extends Controller
         ]);
         $this->loadComponent('Flash');
 
+
         /*
          * Enable the following component for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
