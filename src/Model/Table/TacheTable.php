a321a1b7b324c7674a3eab35d826b56af05a90aa
Thibault Choné <43148335+thibch@users.noreply.github.com>
2020-02-16 20:44:51 +0100

    Retour affichage normal

--- src/Model/Table/TacheTable.php
+++ src/Model/Table/TacheTable.php
@@ -5,10 +5,9 @@ use Cake\ORM\Table;
 
 class TacheTable extends Table{
   public function initialize(array $config){
-    $this->BelongsTo('Utilisateur', [
-      'foreignKey' => 'idResponsable',
-      'propertyName' => 'responsable'
-    ]);
+    $this->BelongsTo('Utilisateur')
+    ->setForeignKey('idResponsable')
+      ->setProperty('responsable');
     $this->BelongsTo('Projet', [
       'foreignKey' => 'idProjet',
       'propertyName' => 'leProjet'
