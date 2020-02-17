57ace43edd9129f6ac87c48fc9cce846c2ab9492
TabaryM <43238044+TabaryM@users.noreply.github.com>
2020-02-17 17:38:08 +0100

    Ajout de JavaDoc sur les fonctions de vérification de formulaire

--- src/Controller/Component/VerificationChamps.php
+++ src/Controller/Component/VerificationChamps.php
@@ -1,14 +1,24 @@
 <?php
-function verification_titre($nom){
+/**
+ * @auth TABARY Mathieu, PALMIERI Adrien
+ * @param $titre String : nom du champs à vérifier
+ * @return bool retourne vrai si le titre correspond aux critères donné dans le cahier des chargess
+ */
+function verification_titre($titre){
     $res = false;
     // Vérification de la taille
-    if(!(strlen($nom) < 1  || strlen($nom) > 128)){
+    if(!(strlen($titre) < 1  || strlen($titre) > 128)){
         $res = true;
     }
 
     return $res;
 }
 
+/**
+ * @auth TABARY Mathieu, PALMIERI Adrien
+ * @param $description String : nom du champs à vérifier
+ * @return bool retourne vrai si la description correspond aux critères donné dans le cahier des chargess
+ */
 function verification_description($description){
     $res = false;
     // Vérification de la taille
