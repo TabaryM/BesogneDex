aac27aa5688df51ef6dffa6595d50873e68c468a
Eir <diianaapop@gmail.com>
2020-02-25 21:54:15 +0100

    Un utilisateur ne faisant pas partie d'un projet ne peut plus accéder à la liste des membres

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -13,6 +13,35 @@ class MembreController extends AppController
     * Auteur : POP Diana
     */
     public function index($id){
+      $estProprietaire = false;
+      $this->loadComponent('Paginator');
+
+      $projetTab = TableRegistry::getTableLocator() //On récupère la table Projet pour en extraire les infos
+        ->get('Projet')->find()
+        ->where(['idProjet' => $id])
+        ->first();
+
+      $session = $this->request->getSession();
+      if ($session->check('Auth.User.idUtilisateur')) {
+        $user = $session->read('Auth.User.idUtilisateur');
+        if($projetTab->idProprietaire == $user){
+          $estProprietaire = true;
+
+        // S'il n'est pas propriétaire, est-il membre ?
+        // -> Vérifie en même temps si le projet existe.
+      }else{
+        $membres = TableRegistry::get('Membre');
+        $query = $membres->find()
+            ->select(['idUtilisateur'])
+            ->where(['idUtilisateur' => $user])
+            ->count();
+            // S'il n'est pas membre non plus, on le redirige.
+        if ($query==0){
+          $this->Flash->error(__('Ce projet n\'existe pas ou vous n\'y avez pas accès.'));
+          $this->redirect(['controller'=>'Accueil', 'action'=>'index']);
+        }
+      }
+
       $this->loadComponent('Paginator');
       $session = $this->request->getSession();
       $membres = $this->Paginator->paginate($this->Membre->find()
@@ -20,6 +49,7 @@ class MembreController extends AppController
           ->where(['idProjet' => $id]));
       $this->set(compact('membres', 'id'));
     }
+  }
 
 
     /**
