9828030c1974972dfaf4a1971b7bd89057022646
Akahiro54 <37707039+Akahiro54@users.noreply.github.com>
2020-02-27 16:29:24 +0100

    ré-ajout d'un bugfix supprimé par michel

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -49,12 +49,18 @@ class ProjetController extends AppController
                       }
                       $projet = $this->Projet->newEntity($receivedData);
                       $session = $this->request->getSession();
-                      $projet->idProprietaire = $session->read('Auth.User.idUtilisateur');
-
+                      $idUser = $session->read('Auth.User.idUtilisateur');
+                      $projet->idProprietaire = $idUser;
+                      foreach($this->Projet->find('all', ['conditions'=>['idProprietaire'=>$idUser]]) as $proj) {
+                          if($proj->titre == $receivedData['titre']) {
+                              $this->Flash->error(__("Impossible d'ajouter un projet avec un nom identique"));
+                              return $this->redirect(['action'=> 'index']);
+                          }
+                      }
                       if ($this->Projet->save($projet)) {
                           $membres = TableRegistry::getTableLocator()->get('Membre');
                           $membre = $membres->newEntity();
-                          $membre->set('idUtilisateur', $session->read('Auth.User.idUtilisateur'));
+                          $membre->set('idUtilisateur', $idUser);
                           $membre->set('idProjet', $projet->idProjet);
 
                           if ($membres->save($membre)) {
