49903d0489ab6fd43976308e2c837c732027b7f8
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-02-27 12:00:16 +0100

    Changement d'état d'une tache

--- webroot/js/tacheTermine.js
+++ webroot/js/tacheTermine.js
@@ -1,4 +1,10 @@
-function che(idTache) {
-  console.log("Tache" + idTache);
-  jQuery("#Tache" + idTache).submit();
-}
+$(document).ready(function() {
+  $('.checkFait').change(function() {
+    var id = $(this).val()
+    if (this.checked) {
+      $.get("/tache/changerEtat/" + id + "/1", null);
+    } else {
+      $.get("/tache/changerEtat/" + id + "/0", null);
+    }
+  });
+});
