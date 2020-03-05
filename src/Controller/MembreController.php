342e97d23c0710761a91efba83db7993f6689c52
djessy.rossi <43148825+djessy1998@users.noreply.github.com>
2020-03-05 11:40:11 +0100

    Fix bug responsable quand on supprime un membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -161,9 +161,7 @@ class MembreController extends AppController
       // Si l'utilisateur sélectionné n'en est pas le propriétaire, il supprime
       }else{
         $taches = TableRegistry::get('Tache');
-        $tachesResponsable = $taches->find()->where(['idProjet'=>$projet->idProjet]);
-
-        debug($tachesResponsable);
+        $tachesResponsable = $taches->updateAll(array('idResponsable' => NULL), ['idProjet'=>$projet->idProjet, 'idResponsable' => $id_utilisateur]);
 
         $membre = $this->Membre->find()->where(['idUtilisateur'=>$id_utilisateur, 'idProjet'=>$id_projet])->first();
         $success = $this->Membre->delete($membre);
