5cae354a26562d0f5b72d64c9324e810388444e2
Thibault Choné <43148335+thibch@users.noreply.github.com>
2020-03-07 16:02:22 +0100

    Ajout de commentaire et doc de index de AccueilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,34 +7,42 @@ use Cake\I18n\Time;
 class AccueilController extends AppController
 {
 
-
+  /**
+  * Index de la page d'accueil, cherche les 10ères tâches prioritaire et les liste dans $tachesPrioritaires
+  * @author Thibault Choné
+  */
   public function index()
   {
     $session = $this->request->getSession();
     if ($session->check('Auth.User.idUtilisateur')) {
       $idUtilisateur = $session->read('Auth.User.idUtilisateur');
-      $today = Time::now();
-      $today->modify('+7 days');
+
+      $dateDans7Jours = Time::now();
+      $dateDans7Jours->modify('+7 days'); //On ajoute 7 jours à la date du jour
 
 
       /*SELECT tache.titre, dateFin
       FROM projet JOIN Tache USING(idProjet)
-      Where idProprietaire == $idUtilisateur and dateFin < $today*/
+      Where idProprietaire == $idUtilisateur and dateFin < $dateDans7Jours*/
 
+      //On recherche toute les tâches des projets qui ont une date qui se finit dans moins de 7 jours
       $tachesResponsable = TableRegistry::getTableLocator()
         ->get('Tache')->find()
         ->select(['titre', 'Projet.dateFin'])
         ->contain('Projet')
         ->distinct()
-        ->where(['idResponsable' => $idUtilisateur, 'Projet.dateFin <' => $today])
+        ->where(['idResponsable' => $idUtilisateur, 'Projet.dateFin <' => $dateDans7Jours])
         ->limit(10)
         ->toArray()
         ;
 
+      //C'est pas ultra propre mais c'est mieux pour le front,
+      //Ici on refait le tableau avec de bon indices (titre, dateFin) au lieu de (titre, leProjet->dateFin)
       $tachesPrioritaires = array();
       foreach($tachesResponsable as $tache) {
         array_push($tachesPrioritaires, ['titre' => $tache['titre'], 'dateFin' => $tache['leProjet']->dateFin]);
       }
+
       $this->set(compact('tachesPrioritaires'));
     }
   }
