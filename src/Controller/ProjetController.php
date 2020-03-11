3e9f0d1e3ba01b694d0d31738fd10e75ed21149f
Théo Roton <theo.roton5@etu.univ-lorraine.fr>
2020-03-11 21:36:56 +0100

    Notification modif projet (c'est po bien de supprimer des choses dans la partie des autres)

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -385,8 +385,38 @@ class ProjetController extends AppController
       if (!$erreur){
         // On sauvegarde les nouvelles informations du projet
         $projets->save($projet);
+
         // On indique que la modification a réussie
         $this->Flash->success(__('Votre projet a été modifé.'));
+
+        //On récupère la table des notifications des projets
+        $notifications = TableRegistry::getTableLocator()->get('Notification_projet');
+
+        //On crée une nouvelle notification pour le projet courant
+        $notification = $notifications->newEntity();
+        $notification->a_valider = 0;
+        $notification->contenu = "Le projet ".$projet->titre." a été modifié.";
+        $notification->idProjet = $receivedData['id'];
+        $notifications->save($notification);
+        $idNot = $notification->idNotificationProjet;
+
+        //On récupère la table de vue des notifications des projets
+        $vue_notifications = TableRegistry::getTableLocator()->get('Vue_notification_projet');
+
+        //On récupère les membres du projet
+        $membres = TableRegistry::getTableLocator()->get('Membre');
+        $membres = $membres->find()->contain('Utilisateur')
+        ->where(['idProjet' => $receivedData['id']]);
+
+        //Pour chaque membre du projet, on envoie une notification à celui-ci
+        foreach ($membres as $m) {
+          $idUtil = $m->un_utilisateur->idUtilisateur;
+
+          $vue_not = $vue_notifications->newEntity();
+          $vue_not->idUtilisateur = $idUtil;
+          $vue_not->idNotifProjet = $idNot;
+          $vue_notifications->save($vue_not);
+        }
 
         // On redirige l'utilisateur sur le projet avec les informations mises à jour
         return $this->redirect(
