969d6c5af0d82e8ad076d02cda8e4485600ed9ae
Théo Roton <theo.roton5@etu.univ-lorraine.fr>
2020-03-12 11:56:15 +0100

    Fix modifier un projet & désarchiver un projet

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -221,14 +221,14 @@ class ProjetController extends AppController
       $session = $this->request->getSession();
       if ($session->check('Auth.User.idUtilisateur')) {
         $user = $session->read('Auth.User.idUtilisateur');
-        if ($projet->dateFin < $now) {
+        if ($projet->etat == 'Expire') {
           if ($user === $projet->idProprietaire) {
-            $projet->etat = "Archive";
+            $projet->etat = 'Archive';
             $projet->dateArchivage = Time::now();
             $this->Projet->save($projet);
 
             // Projet archivé
-            $this->Flash->success(__("Projet achivé avec succès"));
+            $this->Flash->success(__("Projet archivé avec succès"));
             $this->redirect(['action' => 'archives']);
           } else { // Pas le propriétaire
             $this->Flash->error(__("Seul le propriétaire est en mesure d'archiver le projet."));
@@ -240,6 +240,33 @@ class ProjetController extends AppController
         }
       } else {
         $this->Flash->error(__("Vous devez être connecté pour archiver un projet."));
+        $this->redirect($this->referer());
+      }
+    }
+
+    /**
+    * @author Théo Roton
+    */
+    public function desarchive($idProjet) {
+      $projet = $this->Projet->get($idProjet);
+
+      $session = $this->request->getSession();
+      if ($session->check('Auth.User.idUtilisateur')) {
+        $user = $session->read('Auth.User.idUtilisateur');
+        if ($user === $projet->idProprietaire) {
+          $projet->etat = "Expire";
+          $projet->dateArchivage = NULL;
+          $this->Projet->save($projet);
+
+          // Projet archivé
+          $this->Flash->success(__("Projet désarchivé avec succès"));
+          $this->redirect(['action' => 'index']);
+        } else { // Pas le propriétaire
+          $this->Flash->error(__("Seul le propriétaire est en mesure de désarchiver le projet."));
+          $this->redirect($this->referer());
+        }
+      } else {
+        $this->Flash->error(__("Vous devez être connecté pour désarchiver un projet."));
         $this->redirect($this->referer());
       }
     }
@@ -276,13 +303,14 @@ class ProjetController extends AppController
     */
     public function modifierInfos(){
       $receivedData = $this->request->getData();
-      echo "<pre>" , var_dump($receivedData) , "</pre>";
 
       // On récupère le projet pour avoir les anciennes informations
       $projets = TableRegistry::getTableLocator()->get('projet');
       $projet = $projets->find()
       ->where(['idProjet' => $receivedData['id']])
       ->first();
+
+      echo "<pre>" , var_dump($projet->etat) , "</pre>";
 
       $erreur = false;
 
@@ -335,9 +363,11 @@ class ProjetController extends AppController
             $projet->dateDebut = date('Y-m-d',strtotime(implode($receivedData['dateDeb'])));
             $projet->dateFin = date('Y-m-d',strtotime(implode($receivedData['dateFin'])));
 
-            // Si le projet était archivé, et que la nouvelle date de fin est après aujourd'hui, on met le projet en cours
-            if ($projet->etat == 'Archive' && verificationDates($today, $receivedData['dateFin'])) {
+            // Si le projet était archivé ou expiré, et que la nouvelle date de fin est après aujourd'hui, on met le projet en cours
+            if (($projet->etat == 'Archive' || $projet->etat == 'Expire') && verificationDates($today, $receivedData['dateFin'])) {
               $projet->etat = 'En cours';
+            } else {
+              $projet->etat = 'Expire';
             }
 
           } else {
@@ -352,8 +382,8 @@ class ProjetController extends AppController
           $projet->dateDebut = date('Y-m-d',strtotime(implode($receivedData['dateDeb'])));
           $projet->dateFin = null;
 
-          // Si le projet était archivé, alors on le met en cours
-          if ($projet->etat == 'Archive') {
+          // Si le projet était archivé ou expiré, alors on le met en cours
+          if ($projet->etat == 'Archive' || $projet->etat == 'Expire') {
             $projet->etat = 'En cours';
           }
 
