a6b58db6398cd636cabe124dcc61efcdc6343152
Théo Roton <theo.roton5@etu.univ-lorraine.fr>
2020-03-20 12:24:25 +0100

    nouvelles tables

---
+++ src/Model/Table/NotificationTable.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class NotificationTable extends Table {
+
+  public function initialize(array $config){
+    $this->setPrimaryKey('idNotification');
+
+    $this->belongsTo('Projet', [
+      'foreignKey' => 'idProjet',
+      'propertyName' => 'projet_liee'
+    ]);
+
+    $this->belongsTo('Tache', [
+      'foreignKey' => 'idTache',
+      'propertyName' => 'tache_liee'
+    ]);
+
+    $this->hasMany('VueNotification', [
+      'bindingKey' => 'idNotification',
+      'foreignKey' => 'idNotification',
+      'propertyName' => 'une_notification'
+    ]);
+  }
+
+}
+
+?>
